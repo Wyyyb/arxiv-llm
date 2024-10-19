3b46ef3821a944df8d4344d830d714569a690954
Wyyyb <yubo.wang.sunny@gmail.com>
2024-10-19 18:38:17 +0800

    add

--- arxivllm/train_01.sh
+++ arxivllm/train_01.sh
@@ -8,7 +8,7 @@ deepspeed --include localhost:0,1,2,3,4,5,6,7 --master_port 60000 train.py \
   --model_name_or_path /gpfs/public/research/xy/yubowang/models/Qwen2.5-7B \
   --save_steps 500 \
   --dataset_name json \
-  --dataset_path /data/yubowang/cite_llm/data/train_data_1016_0.jsonl \
+  --dataset_path ../data/train_data_1016_0.jsonl \
   --bf16 \
   --normalize \
   --temperature 0.01 \
