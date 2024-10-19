c25f327023416268f263baf6fda46cf347d0da4f
Wyyyb <yubo.wang.sunny@gmail.com>
2024-10-19 19:25:27 +0800

    add

--- data_preprocess/filter_and_merge_data_new.py
+++ data_preprocess/filter_and_merge_data_new.py
@@ -6,27 +6,13 @@ import time
 import torch
 
 
-def is_within_token_limit(input_string, max_tokens=15000):
-    # 加载Qwen2.5-7B的tokenizer
-    tokenizer = AutoTokenizer.from_pretrained("/gpfs/public/research/xy/yubowang/models/Qwen2.5-7B")
-
-    # 对输入字符串进行编码
-    encoded = tokenizer.encode(input_string, add_special_tokens=True)
-
-    # 获取token数量
-    token_count = len(encoded)
-
-    # 判断token数是否在限制范围内
-    return token_count <= max_tokens
-
-
 def batch_process_gpu(texts, tokenizer, max_tokens=15000, batch_size=64):
     results = []
     start_time = time.time()
 
     # 将处理移动到GPU
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print("device", device)
     for i in range(0, len(texts), batch_size):
         batch = texts[i:i + batch_size]
         encoded = tokenizer(batch, add_special_tokens=True, padding=True, truncation=True, return_tensors="pt")
@@ -43,6 +29,7 @@ def batch_process_gpu(texts, tokenizer, max_tokens=15000, batch_size=64):
 
 
 def process_data(input_dir="../data_bk/", output_dir="../data/"):
+    tokenizer = AutoTokenizer.from_pretrained("/gpfs/public/research/xy/yubowang/models/Qwen2.5-7B")
     os.makedirs(output_dir, exist_ok=True)
     train_data = []
     val_data = []
@@ -52,22 +39,30 @@ def process_data(input_dir="../data_bk/", output_dir="../data/"):
         print("Processing", file)
         if not file.endswith(".jsonl"):
             continue
+        tmp = []
         if file.startswith("train"):
             with open(file_path, "r") as fi:
                 for line in tqdm(fi.readlines()):
                     curr = json.loads(line)
-                    if is_within_token_limit(curr["paper"]):
-                        train_data.append(curr)
-                    else:
-                        exceed_number += 1
+                    tmp.append(curr)
+            tmp_res = batch_process_gpu(tmp, tokenizer)
+            for each_res in tmp_res:
+                if not each_res:
+                    exceed_number += 1
+                    continue
+                train_data.append(each_res)
         elif file.startswith("val"):
             with open(file_path, "r") as fi:
                 for line in tqdm(fi.readlines()):
                     curr = json.loads(line)
-                    if is_within_token_limit(curr["paper"]):
-                        val_data.append(curr)
-                    else:
-                        exceed_number += 1
+                    tmp.append(curr)
+            tmp_res = batch_process_gpu(tmp, tokenizer)
+            for each_res in tmp_res:
+                if not each_res:
+                    exceed_number += 1
+                    continue
+                val_data.append(each_res)
+
     train_output_path = os.path.join(output_dir, "train_data_1019.jsonl")
     val_output_path = os.path.join(output_dir, "val_data_1019.jsonl")
     print("exceed num", exceed_number)
