51ab8f3b28fe12819991ee090fa49356c33fb14c
Wyyyb <yubo.wang.sunny@gmail.com>
2024-10-20 23:44:54 +0800

    add

--- data_preprocess/generate_corpus.py
+++ data_preprocess/generate_corpus.py
@@ -3,16 +3,28 @@ import os
 from tqdm import tqdm
 
 
+def check_cs(categories):
+    cat = categories.lower()
+    cat = cat.replace("physics", "")
+    if "cs." in cat:
+        return True
+    else:
+        return False
+
+
 def generate_corpus(file_path):
-    output_path = "../data/meta_data_1020.jsonl"
+    output_path = "../corpus_data/meta_data_1020.jsonl"
     res = []
     with open(file_path, "r") as fi:
         for line in tqdm(fi.readlines()):
             curr = json.loads(line)
+            if not check_cs(curr["categories"]):
+                continue
             docs_id = curr["id"]
             title = curr["title"]
             abstract = "<|reference_start|>" + curr["abstract"].strip() + "<|reference_end|>"
             res.append({"docs_id": docs_id, "title": title, "abstract": abstract})
+    print("length of res", len(res))
     with open(output_path, "w") as fo:
         for each in res:
             fo.write(json.dumps(each))
