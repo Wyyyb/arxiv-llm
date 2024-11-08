34f4164e32f4790aa97bf5b1a9854a55a4fa522d
Wyyyb <yubo.wang.sunny@gmail.com>
2024-11-08 23:14:05 +0800

    add

--- utils/update_corpus_1103.py
+++ utils/update_corpus_1103.py
@@ -6,11 +6,11 @@ with open("../corpus_data/meta_data_1022.jsonl", "r") as fi:
     for line in fi.readlines():
         curr = json.loads(line)
         curr["abstract"] = curr["abstract"].replace("<|reference_start|>", "<|reference_start|> ")\
-            .replace("<|reference_end|>", " <|reference_end>")
+            .replace("<|reference_end|>", " <|reference_end|>")
         data.append(curr)
 
 print("length of meta data", len(data))
-with open("../corpus_data/meta_data_1103.jsonl", "w") as fo:
+with open("../corpus_data/meta_data_1108.jsonl", "w") as fo:
     for each in data:
         fo.write(json.dumps(each))
         fo.write("\n")
