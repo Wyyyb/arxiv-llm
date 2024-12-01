f353e9682765cd42d61349ffe62c318cac10bc9c
Wyyyb <yubo.wang.sunny@gmail.com>
2024-12-01 18:08:46 +0800

    add

--- scripts/inference_pipeline_14B.sh
+++ scripts/inference_pipeline_14B.sh
@@ -1,11 +1,11 @@
 source /gpfs/public/research/miniconda3/bin/activate
 conda activate cite_rag
 
-export CUDA_VISIBLE_DEVICES=0,1,2,3
+export CUDA_VISIBLE_DEVICES=1
 
 cd ../arxivllm
 
-python inference_pipeline_1130_14B.py
+python inference_pipeline_1130_14B_test.py
 
 
 
