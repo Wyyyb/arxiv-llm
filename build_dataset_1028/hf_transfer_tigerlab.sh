ef1be5edb858b6c73b5c9ee940d4e2d86103a62c
Wyyyb <yubo.wang.sunny@gmail.com>
2024-12-08 00:21:08 +0800

    add

--- build_dataset_1028/hf_transfer_tigerlab.sh
+++ build_dataset_1028/hf_transfer_tigerlab.sh
@@ -1,17 +1,27 @@
-cd
+mkdir -p ../scholar_copilot_data
+cd ../scholar_copilot_data
 
-huggingface-cli upload TIGER-Lab/ . --repo-type dataset
+cp ../embedded_corpus/scholar-hnsw-1207/index ./
+cp ../embedded_corpus/scholar-hnsw-1207/lookup_indices.npy ./
+cp ../corpus_data/corpus_data_arxiv_1129.jsonl ./
+cp ../local_bibtex_info/bibtex_info_1202.jsonl ./
 
-huggingface-cli download ubowang/embedded_corpus_1207 --local-dir . --repo-type dataset
-
-
-
-huggingface-cli upload ubowang/scholar-hnsw-1207 . --repo-type dataset
-
-
-huggingface-cli download ubowang/scholar-hnsw-1207 --local-dir . --repo-type dataset
-
-
-
-
-
+huggingface-cli upload TIGER-Lab/ScholarCopilot-Data-v1208 . --repo-type dataset
+#
+#cd /gpfs/public/research/xy/yubowang/arxiv-llm/embedded_corpus/scholar-hnsw-1207/
+#huggingface-cli upload TIGER-Lab/scholar-hnsw-1207 . --repo-type dataset
+#
+#
+#cd /gpfs/public/research/xy/yubowang/arxiv-llm/model_output/v1127_multi_cite/checkpoint-2000
+#huggingface-cli upload TIGER-Lab/ScholarCopilot-v1208 . --repo-type model
+#
+#
+#
+#
+#mkdir -p ../embedded_corpus
+#mkdir -p ../embedded_corpus/scholar-hnsw-1207
+#huggingface-cli download TIGER-Lab/scholar-hnsw-1207 --local-dir ../embedded_corpus/scholar-hnsw-1207 --repo-type dataset
+#
+#mkdir -p ../model_output
+#mkdir -p ../model_output/v1208
+#
