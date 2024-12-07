9a7c88372bbb631642b3752f99a380b904a9e985
Wyyyb <yubo.wang.sunny@gmail.com>
2024-12-07 13:17:56 +0800

    add

--- scholar_copilot/scholar_copilot_gradio_1206.py
+++ scholar_copilot/scholar_copilot_gradio_1206.py
@@ -324,7 +324,7 @@ with gr.Blocks() as app:
 if __name__ == "__main__":
     # model_path = "/gpfs/public/research/xy/yubowang/arxiv-llm/model_output/v1127_multi_cite/checkpoint-2000/"
     model_path = "/data/yubowang/arxiv-llm/model_output/v1127_multi_cite/checkpoint-2000/"
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
     model, tokenizer = load_model(model_path, device)
     embedded_corpus_path = "../embedded_corpus/1129_shards/"
     # encoded_corpus, lookup_indices = load_corpus_base(embedded_corpus_path)
