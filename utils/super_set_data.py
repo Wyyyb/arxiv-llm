efbac5f7119112282681ea2ada2744a0e6dc9086
Wyyyb <yubo.wang.sunny@gmail.com>
2024-12-15 16:22:17 +0800

    add

---
+++ utils/super_set_data.py
@@ -0,0 +1,30 @@
+import json
+
+
+with open("../scholar_copilot_data/bibtex_info_1202.jsonl", "r") as fi:
+    bibtex_info = {}
+    for line in fi:
+        curr = json.loads(line)
+        paper_id = curr['id']
+        bibtex_info[paper_id] = curr
+
+with open("../scholar_copilot_data/corpus_data_arxiv_1129.jsonl", "r") as fi:
+    corpus_data = []
+    for line in fi:
+        curr = json.loads(line)
+        paper_id = curr["paper_id"]
+        if paper_id not in bibtex_info:
+            print("Could not find paper", paper_id)
+            continue
+        bibtex = bibtex_info[paper_id]["bibtex"]
+        # title = bibtex_info[paper_id]["title"]
+        citation_key = bibtex_info[paper_id]["citation_key"]
+        curr["bibtex"] = bibtex
+        curr["citation_key"] = citation_key
+    corpus_data.append(curr)
+
+with open("../scholar_copilot_data/corpus_data_arxiv_1215.jsonl", "w") as fo:
+    for each in corpus_data:
+        fo.write(json.dumps(each) + "\n")
+
+
