4a6691064e1d5884e23546e1ea9adfa368bc808f
Wyyyb <yubo.wang.sunny@gmail.com>
2024-12-15 16:38:25 +0800

    add

--- utils/super_set_data.py
+++ utils/super_set_data.py
@@ -1,16 +1,17 @@
 import json
+from tqdm import tqdm
 
 
 with open("../scholar_copilot_data_1215/bibtex_info_1202.jsonl", "r") as fi:
     bibtex_info = {}
-    for line in fi:
+    for line in tqdm(fi.readlines()):
         curr = json.loads(line)
         paper_id = curr['id']
         bibtex_info[paper_id] = curr
 
 with open("../scholar_copilot_data_1215/corpus_data_arxiv_1129.jsonl", "r") as fi:
     corpus_data = []
-    for line in fi:
+    for line in tqdm(fi.readlines()):
         curr = json.loads(line)
         paper_id = curr["paper_id"]
         if paper_id not in bibtex_info:
@@ -25,7 +26,7 @@ with open("../scholar_copilot_data_1215/corpus_data_arxiv_1129.jsonl", "r") as f
     corpus_data.append(curr)
 
 with open("../scholar_copilot_data_1215/corpus_data_arxiv_1215.jsonl", "w") as fo:
-    for each in corpus_data:
+    for each in tqdm(corpus_data):
         fo.write(json.dumps(each) + "\n")
 
 
